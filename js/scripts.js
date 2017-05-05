36355bf679a10f9cdbbdcc308bc00112abdfbaf6
gichuki8888 <gichuky@yahoo.com>
2017-05-05 17:47:32 +0300

    adjustment

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var xbox = function(yellow) {
-      for (var i = 0; i <= yellow; i++) {
+  var yellow = parseInt($("input#yellow").val());
+      for (var i = 1; i <= yellow; i++) {
         if (i % 3 === 0) {
           $("ul#result").append("<li> ping</li>");
         } else if (i % 5 === 0) {
@@ -9,14 +10,15 @@ var xbox = function(yellow) {
         } else {
           $("ul#result").append("<li>"+ String(i)+"</li>");
 
-        }
-      }
-    }
+        };
+      };
+    };
 
 
 $(document).ready(function() {
   $("form#xbox").submit(function(event) {
     event.preventDefault();
-    var yellow = parseInt($("input#yellow").val())
+
+    xbox()
 });
 });
