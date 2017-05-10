73d65b967ef26872eee532f5bb838fddf914c033
gichuki8888 <gichuky@yahoo.com>
2017-05-10 09:17:05 +0300

    script adjustment

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 var xbox = function(yellow) {
   var yellow = parseInt($("input#yellow").val());
       for (var i = 1; i <= yellow; i++) {
-        if (i % 3 === 0) {
+         if ((i % 3 === 0) && (i % 5 === 0)) {
+          $("ul#result").append("<li> pingpong</li>");
+         }
+        else if (i % 3 === 0) {
           $("ul#result").append("<li> ping</li>");
         } else if (i % 5 === 0) {
           $("ul#result").append("<li> pong</li>");
-        } else if (i % 15 === 0) {
-          $("ul#result").append("<li> pingpong</li>");
-        } else {
+        }
+         else {
           $("ul#result").append("<li>"+ String(i)+"</li>");
 
         };
